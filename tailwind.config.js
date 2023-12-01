1bce3ebce29d449eacf5588ffe577d0f18a2709e
MattTeerapat <mmatt.trp@gmail.com>
2023-12-01 13:52:03 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+// module.exports = {
+//   content: [
+//     "./index.html",
+//     "./src/main.jsx",
+//     "./src/components/ProejectSideBar.jsx",
+//     "./src/App.jsx",
+//     "./src/components/NewProject.jsx",
+//     "./src/components/Input.jsx",
+//     "./src/components/NoProjectSelected.jsx",
+//     "./src/components/Button.jsx",
+//     "./src/components/Modal.jsx",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
